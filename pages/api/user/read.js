18f50abc53e4f6ffaf008a2d863b00e2f8e8302e
Tri Cahya Fitriansyah <tricahya644@gmail.com>
2023-11-19 21:16:29 +0700

    backend

---
+++ pages/api/user/read.js
@@ -0,0 +1,15 @@
+import prisma from '../../prismadb';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const items = await prisma.model.findMany();
+      res.status(200).json(items);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error fetching items' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
