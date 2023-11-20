360837a98f27de8e259c597a575805efd51bb294
xyla <xyla@gmail.com>
2023-11-20 21:34:42 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          colors: {
+            'steel-blue': '#155e75',
+          },
       },
     },
   },
