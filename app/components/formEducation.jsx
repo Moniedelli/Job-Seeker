036913fa45d9e43c7180e4f21852f6bcf01e2ddf
Adel <@ariwidaniadelia@gmail.com>
2023-11-21 23:43:06 +0800

    migrate API to next

--- app/components/formEducation.jsx
+++ app/components/formEducation.jsx
@@ -1,41 +1,94 @@
+'use client'
 
-'use client';
-
-import { Datepicker, Button, Checkbox, Label, TextInput, Select } from 'flowbite-react';
+import axios from 'axios';
+import { useState } from 'react';
+import { Datepicker, Button, Label, TextInput } from 'flowbite-react';
 
 function FormEducation() {
+  const [lastEdu, setLastEdu] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [finishDate, setFinishDate] = useState(new Date());
+  const [grade, setGrade] = useState('');
+  const [organization, setOrganization] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      lastEdu,
+      startDate,
+      finishDate,
+      grade,
+      organization
+    };
+
+    try {
+      const response = await axios.post('/api/userEdu/create', formData);
+      console.log(response.data); // Handle response as needed
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      // Handle error as needed
+    }
+  };
+
   return (
-    <div> 
-      <form className="flex max-w-md flex-col gap-4">
+    <div>
+      <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmit}>
         <div>
           <div className="mb-2 block">
-            <Label htmlFor="email1" value="Pendidikan Terakhir" />
+            <Label htmlFor="lastEdu" value="Pendidikan Terakhir" />
           </div>
-          <TextInput id="email" type="email" placeholder="name@gmail.com" required />
+          <TextInput
+            id="lastEdu"
+            type="text"
+            placeholder="Pendidikan Terakhir"
+            value={lastEdu}
+            onChange={(e) => setLastEdu(e.target.value)}
+            required
+          />
         </div>
         <div>
           <div className="mb-2 block">
-            <Label htmlFor="base" value="Tahun Mulai" />
-            <Datepicker />
+            <Label htmlFor="startDate" value="Tahun Mulai" />
+            <Datepicker
+              id="startDate"
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+            />
           </div>
         </div>
         <div>
           <div className="mb-2 block">
-            <Label htmlFor="base" value="Tahun selesai" />
-            <Datepicker />
+            <Label htmlFor="finishDate" value="Tahun Selesai" />
+            <Datepicker
+              id="finishDate"
+              selected={finishDate}
+              onChange={(date) => setFinishDate(date)}
+            />
           </div>
         </div>
         <div>
           <div className="mb-2 block">
-            <Label htmlFor="base" value="Nilai" />
+            <Label htmlFor="grade" value="Nilai" />
           </div>
-          <TextInput id="address" type="text" sizing="md" />
+          <TextInput
+            id="grade"
+            type="text"
+            placeholder="Nilai"
+            value={grade}
+            onChange={(e) => setGrade(e.target.value)}
+          />
         </div>
         <div>
           <div className="mb-2 block">
-            <Label htmlFor="base" value="Organisasi" />
+            <Label htmlFor="organization" value="Organisasi" />
           </div>
-          <TextInput id="base" type="text" sizing="md" />
+          <TextInput
+            id="organization"
+            type="text"
+            placeholder="Organisasi"
+            value={organization}
+            onChange={(e) => setOrganization(e.target.value)}
+          />
         </div>
         <Button type="submit">Submit</Button>
       </form>
